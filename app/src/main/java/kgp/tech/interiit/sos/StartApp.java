dae4c9c550db2287f49e0607648c1823ac696353
Akshay Gupta <akshaysngupta@gmail.com>
2016-01-17 11:52:24 +0530

    Push notifcations added

--- app/src/main/java/kgp/tech/interiit/sos/StartApp.java
+++ app/src/main/java/kgp/tech/interiit/sos/StartApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.parse.Parse;
 import com.parse.ParseACL;
+import com.parse.ParseInstallation;
 import com.parse.ParseUser;
 
 
@@ -18,8 +19,8 @@ public class StartApp extends Application {
 
         // Add your initialization code here
         Parse.initialize(this);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
 
-        ParseUser.enableAutomaticUser();
         ParseACL defaultACL = new ParseACL();
         // Optionally enable public read access.
         // defaultACL.setPublicReadAccess(true);
