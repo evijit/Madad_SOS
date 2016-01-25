a9d6793e3773c6763a8c53018931dc13aaf4334a
Avijit Ghosh <avijitg22@gmail.com>
2016-01-26 00:25:22 +0530

    Scrollable details pane on Chat View

--- app/src/main/java/kgp/tech/interiit/sos/MessageActivity.java
+++ app/src/main/java/kgp/tech/interiit/sos/MessageActivity.java
@@ -1,23 +1,28 @@
 package kgp.tech.interiit.sos;
-/**
- * Created by Avijit Ghosh on 24-02-2015.
- */
 
-import android.app.AlertDialog;
+
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.SpannableString;
-import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.ksoichiro.android.observablescrollview.ObservableListView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
+import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
 import com.pubnub.api.Callback;
 import com.pubnub.api.Pubnub;
 import com.pubnub.api.PubnubError;
@@ -30,16 +35,20 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.UUID;
 
-//import android.app.ListActivity;
+public class MessageActivity extends BaseActivity implements ObservableScrollViewCallbacks {
 
-/**
- * MessageActivity is a main Activity to show a ListView containing Message items
- *
- * @author Adil Soomro
- *
- */
-public class MessageActivity extends AppCompatActivity {
-    /** Called when the activity is first created. */
+    private static final float MAX_TEXT_SCALE_DELTA = 0.3f;
+
+    private View mImageView;
+    private View mOverlayView;
+    private View mListBackgroundView;
+    private TextView mTitleView;
+    private View mFab;
+    private int mActionBarSize;
+    private int mFlexibleSpaceShowFabOffset;
+    private int mFlexibleSpaceImageHeight;
+    private int mFabMargin;
+    private boolean mFabIsShown;
 
     ArrayList<Message> messages;
     AwesomeAdapter adapter;
@@ -48,16 +57,17 @@ public class MessageActivity extends AppCompatActivity {
     static String sender;
     private Toolbar toolbar;
     static String message_incoming = "";
-    private ListView listView;
+    private ObservableListView listView;
     static UUID uuid_this = UUID.randomUUID();
     final Pubnub pubnub = new Pubnub("pub-c-f9d02ea4-19f1-4737-b3e1-ef2ce904b94f", "sub-c-3d547124-be29-11e5-8a35-0619f8945a4f");
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message);
 
-        /* Instantiate PubNub */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolltest);
+
+
         pubnub.setUUID(uuid_this);
         try {
             pubnub.subscribe("Channel-ag04qto2e", new Callback() {
@@ -89,52 +99,15 @@ public class MessageActivity extends AppCompatActivity {
             System.out.println(e.toString());
         }
 
-        toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
-        SpannableString s = new SpannableString("");
-        if(toolbar != null)
-        {
-            setSupportActionBar(toolbar);
-            getSupportActionBar().setTitle(s);
-            getSupportActionBar().setHomeButtonEnabled(true);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            //sender = Utility.sender[rand.nextInt( Utility.sender.length-1)];
-
-        }
-
         Intent iin= getIntent();
         Bundle b = iin.getExtras();
 
-        if(b!=null && toolbar!=null)
-        {
-            String j =(String) b.get("name");
-            SpannableString s1 = new SpannableString(j);
-            TextView title= (TextView)findViewById(R.id.toolbar_title);
-            title.setText(j);
-            toolbar.findViewById(R.id.toolbar_title).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MessageActivity.this);
-// ...Irrelevant code for customizing the buttons and title
-                    LayoutInflater inflater = MessageActivity.this.getLayoutInflater();
-                    View dialogView = inflater.inflate(R.layout.contact_card, null);
-                    dialogBuilder.setView(dialogView);
-
-
-                    AlertDialog alertDialog = dialogBuilder.create();
-                    alertDialog.show();
-                    // Toast.makeText(getApplicationContext(), "CONTACTS CARD TO BE SHOWN. WORK IN PROGRESS.",
-                    //         Toast.LENGTH_LONG).show();
-                }
-            });
-            //getSupportActionBar().setTitle(s1);
-            sender=j;
-        }
-
-        listView = (ListView) findViewById(R.id.list);
+        String j =(String) b.get("name");
+        TextView title= (TextView)findViewById(R.id.name);
+        title.setText(j);
+        sender=j;
+        listView = (ObservableListView) findViewById(R.id.list);
         text = (EditText) this.findViewById(R.id.text);
-
-        /*sender = Utility.sender[rand.nextInt( Utility.sender.length-1)];
-        this.setTitle(sender);*/
         messages = new ArrayList<Message>();
 
         messages.add(new Message("Hello", false));
@@ -148,7 +121,132 @@ public class MessageActivity extends AppCompatActivity {
         adapter = new AwesomeAdapter(this, messages);
         listView.setAdapter(adapter);
         addNewMessage(new Message("mmm, well, using 9 patches png to show them.", true));
+
+
+        mFlexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
+        mFlexibleSpaceShowFabOffset = getResources().getDimensionPixelSize(R.dimen.flexible_space_show_fab_offset);
+        mActionBarSize = getActionBarSize();
+        mImageView = findViewById(R.id.image);
+        mOverlayView = findViewById(R.id.overlay);
+        //ObservableListView listView = (ObservableListView) findViewById(R.id.list);
+        listView.setScrollViewCallbacks(this);
+
+        // Set padding view for ListView. This is the flexible space.
+        View paddingView = new View(this);
+        AbsListView.LayoutParams lp = new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT,
+                mFlexibleSpaceImageHeight);
+        paddingView.setLayoutParams(lp);
+
+        // This is required to disable header's list selector effect
+        paddingView.setClickable(true);
+
+        listView.addHeaderView(paddingView);
+        //setDummyData(listView);
+        mTitleView = (TextView) findViewById(R.id.title);
+        mTitleView.setText(getTitle());
+        setTitle(null);
+        mFab = findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MessageActivity.this, "FAB is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFabMargin = getResources().getDimensionPixelSize(R.dimen.margin_standard);
+        ViewHelper.setScaleX(mFab, 0);
+        ViewHelper.setScaleY(mFab, 0);
+
+        // mListBackgroundView makes ListView's background except header view.
+        mListBackgroundView = findViewById(R.id.list_background);
     }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        // Translate overlay and image
+        float flexibleRange = mFlexibleSpaceImageHeight - mActionBarSize;
+        int minOverlayTransitionY = mActionBarSize - mOverlayView.getHeight();
+        ViewHelper.setTranslationY(mOverlayView, ScrollUtils.getFloat(-scrollY, minOverlayTransitionY, 0));
+        ViewHelper.setTranslationY(mImageView, ScrollUtils.getFloat(-scrollY / 2, minOverlayTransitionY, 0));
+
+        // Translate list background
+        ViewHelper.setTranslationY(mListBackgroundView, Math.max(0, -scrollY + mFlexibleSpaceImageHeight));
+
+        // Change alpha of overlay
+        ViewHelper.setAlpha(mOverlayView, ScrollUtils.getFloat((float) scrollY / flexibleRange, 0, 1));
+
+        // Scale title text
+        float scale = 1 + ScrollUtils.getFloat((flexibleRange - scrollY) / flexibleRange, 0, MAX_TEXT_SCALE_DELTA);
+        setPivotXToTitle();
+        ViewHelper.setPivotY(mTitleView, 0);
+        ViewHelper.setScaleX(mTitleView, scale);
+        ViewHelper.setScaleY(mTitleView, scale);
+
+        // Translate title text
+        int maxTitleTranslationY = (int) (mFlexibleSpaceImageHeight - mTitleView.getHeight() * scale);
+        int titleTranslationY = maxTitleTranslationY - scrollY;
+        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
+
+        // Translate FAB
+        int maxFabTranslationY = mFlexibleSpaceImageHeight - mFab.getHeight() / 2;
+        float fabTranslationY = ScrollUtils.getFloat(
+                -scrollY + mFlexibleSpaceImageHeight - mFab.getHeight() / 2,
+                mActionBarSize - mFab.getHeight() / 2,
+                maxFabTranslationY);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            // On pre-honeycomb, ViewHelper.setTranslationX/Y does not set margin,
+            // which causes FAB's OnClickListener not working.
+            FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mFab.getLayoutParams();
+            lp.leftMargin = mOverlayView.getWidth() - mFabMargin - mFab.getWidth();
+            lp.topMargin = (int) fabTranslationY;
+            mFab.requestLayout();
+        } else {
+            ViewHelper.setTranslationX(mFab, mOverlayView.getWidth() - mFabMargin - mFab.getWidth());
+            ViewHelper.setTranslationY(mFab, fabTranslationY);
+        }
+
+        // Show/hide FAB
+        if (fabTranslationY < mFlexibleSpaceShowFabOffset) {
+            hideFab();
+        } else {
+            showFab();
+        }
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void setPivotXToTitle() {
+        Configuration config = getResources().getConfiguration();
+        if (Build.VERSION_CODES.JELLY_BEAN_MR1 <= Build.VERSION.SDK_INT
+                && config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+            ViewHelper.setPivotX(mTitleView, findViewById(android.R.id.content).getWidth());
+        } else {
+            ViewHelper.setPivotX(mTitleView, 0);
+        }
+    }
+
+    private void showFab() {
+        if (!mFabIsShown) {
+            ViewPropertyAnimator.animate(mFab).cancel();
+            ViewPropertyAnimator.animate(mFab).scaleX(1).scaleY(1).setDuration(200).start();
+            mFabIsShown = true;
+        }
+    }
+
+    private void hideFab() {
+        if (mFabIsShown) {
+            ViewPropertyAnimator.animate(mFab).cancel();
+            ViewPropertyAnimator.animate(mFab).scaleX(0).scaleY(0).setDuration(200).start();
+            mFabIsShown = false;
+        }
+    }
+
     public void sendMessage(View v)
     {
         String newMessage = text.getText().toString().trim();
@@ -239,28 +337,6 @@ public class MessageActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle presses on the action bar items
-//        switch (item.getItemId()) {
-//
-//            case R.id.action_attach:
-//                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//                intent.setType("*/*");
-//                startActivityForResult(intent,1);
-//
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//    }
-
-    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // TODO Auto-generated method stub
         switch(requestCode){
@@ -275,5 +351,4 @@ public class MessageActivity extends AppCompatActivity {
 
         }
     }
-
 }
