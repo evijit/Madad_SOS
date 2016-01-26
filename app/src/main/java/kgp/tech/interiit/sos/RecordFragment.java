c95f7b2eee607f6d58e8b48df4a9d7e3a298c6fb
Shivam Vats <shivamvats.iitkgp@gmail.com>
2016-01-27 00:14:05 +0530

    Added buttons and text for audio recording

--- app/src/main/java/kgp/tech/interiit/sos/RecordFragment.java
+++ app/src/main/java/kgp/tech/interiit/sos/RecordFragment.java
@@ -5,6 +5,9 @@ package kgp.tech.interiit.sos;
  */
 
 import android.app.Activity;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.os.Bundle;
 import android.os.Environment;
@@ -17,7 +20,13 @@ import android.util.Log;
 import android.media.MediaRecorder;
 import android.media.MediaPlayer;
 
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
 import java.io.IOException;
+
+import kgp.tech.interiit.sos.Utils.Utils;
 
 
 public class RecordFragment extends Activity
@@ -25,11 +34,14 @@ public class RecordFragment extends Activity
     private static final String LOG_TAG = "AudioRecordTest";
     private static String mFileName = null;
 
-    private RecordButton mRecordButton = null;
+    private Button mRecordButton = null;
     private MediaRecorder mRecorder = null;
 
-    private PlayButton   mPlayButton = null;
+    private Button   mPlayButton = null;
     private MediaPlayer   mPlayer = null;
+
+    boolean mStartRecording = true;
+    boolean mStartPlaying = true;
 
     private void onRecord(boolean start) {
         if (start) {
@@ -85,50 +97,6 @@ public class RecordFragment extends Activity
         mRecorder = null;
     }
 
-    class RecordButton extends Button {
-        boolean mStartRecording = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onRecord(mStartRecording);
-                if (mStartRecording) {
-                    setText("Stop recording");
-                } else {
-                    setText("Start recording");
-                }
-                mStartRecording = !mStartRecording;
-            }
-        };
-
-        public RecordButton(Context ctx) {
-            super(ctx);
-            setText("Start recording");
-            setOnClickListener(clicker);
-        }
-    }
-
-    class PlayButton extends Button {
-        boolean mStartPlaying = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onPlay(mStartPlaying);
-                if (mStartPlaying) {
-                    setText("Stop playing");
-                } else {
-                    setText("Start playing");
-                }
-                mStartPlaying = !mStartPlaying;
-            }
-        };
-
-        public PlayButton(Context ctx) {
-            super(ctx);
-            setText("Start playing");
-            setOnClickListener(clicker);
-        }
-    }
-
     public RecordFragment() {
         mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
         mFileName += "/audiorecordtest.3gp";
@@ -138,24 +106,71 @@ public class RecordFragment extends Activity
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
-        LinearLayout ll = new LinearLayout(this);
+        setContentView(R.layout.fragment_record);
 
-        mRecordButton = new RecordButton(this);
-        ll.addView(mRecordButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        mPlayButton = new PlayButton(this);
-        ll.addView(mPlayButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        setContentView(ll);
-        ViewGroup.LayoutParams params = ll.getLayoutParams();
-        params.height=500;
-        params.width=500;
+        mRecordButton = (Button) findViewById(R.id.record);
+        mRecordButton.setText("Start recording");
+        mRecordButton.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                onRecord(mStartRecording);
+                if (mStartRecording) {
+                    ((Button) v).setText("Stop recording");
+                } else {
+                    ((Button) v).setText("Start recording");
+                }
+                mStartRecording = !mStartRecording;
+            }
+        });
+
+        mPlayButton = (Button) findViewById(R.id.play);
+        mPlayButton.setText("Start playing");
+        mPlayButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                onPlay(mStartPlaying);
+                if (mStartPlaying) {
+                    ((Button)v).setText("Stop playing");
+                } else {
+                    ((Button)v).setText("Start playing");
+                }
+                mStartPlaying = !mStartPlaying;
+            }
+        });
+
+        final EditText mTextBox = (EditText) findViewById(R.id.message);
+        Button skip = (Button) findViewById(R.id.skip);
+        Button save = (Button) findViewById(R.id.save);
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RecordFragment.this, AnimatedButtons.class));
+                finish();
+            }
+
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = mTextBox.getText().toString();
+                if (message.length() == 0) {
+                    Utils.showDialog(RecordFragment.this, getString(R.string.err_fields_empty));
+                    return;
+                }
+                ParseObject parseObject = new ParseObject("SOS");
+                parseObject.put("Description", message);
+                ParseFile audio = new ParseFile();
+
+                parseObject.put("audio",audio);
+                parseObject.saveInBackground();
+                startActivity(new Intent(RecordFragment.this, AnimatedButtons.class));
+                finish();
+            }
+
+        });
     }
 
     @Override
